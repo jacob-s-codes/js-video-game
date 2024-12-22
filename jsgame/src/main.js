4fbf16967804e130531e23df17fa62d61a3351b3
jacob-s-codes <jacobbshaul@gmail.com>
2024-12-22 00:32:08 -0800

    moved index because that was breaking stuff

--- jsgame/src/main.js
+++ jsgame/src/main.js
@@ -1,53 +1,30 @@
-import kaplay from "kaplay";
-import "kaplay/global"; // uncomment if you want to use without the k. prefix
-
-const k = kaplay();
-k.setGravity(2000);
-
-k.loadRoot("./"); // A good idea for Itch.io publishing later
+import kaplay from "kaplay"; 
+import "kaplay/global"; // uncomment if you want to use without the k. prefix 
+const k = kaplay(); k.setGravity(2000); 
+k.loadRoot("./"); // A good idea for Itch.io publishing later 
 k.loadSprite("bean", "sprites/bean.png");
 
 const player = k.add([
-    k.sprite("bean"),
-    k.pos(center()),
-    k.area(),
-    k.body(),
+    k.sprite("bean"), 
+    k.pos(center()), 
+    k.area(), 
+    k.body(), 
     k.offscreen(),
-])      
+])
 
-k.add([
-    k.rect(width(), 300),
-    k.pos(0, 800),
+const rect = k.add([
+    k.rect(800, 800),
+    k.pos(300, 800),
     k.area(),
     k.body({ isStatic: true}),
 ])
 
-let counter = 0;
-let counterUI = k.add([k.text("1")]);
-
-k.loop(1, () => {
-    counter+=2;
-    counterUI.text = counter;
-    const speeds = [300, 500, 800];
-    const currentSpeed = speeds[Math.floor(Math.random()*speeds.length)];
-
-    k.add([
-        k.rect(50, 50), 
-        k.pos(600, 800),
-        k.area(),
-        k.body(),
-        k.outline(),
-        k.move(k.vec2(-1,0), currentSpeed),
-    ])
-})
-
-player.onKeyPress("space", () => { 
+onKeyPress("space", () => {
     if (player.isGrounded()) {
         player.jump();
     }
 });
 
-player.onExitScreen(() => {
-    k.add([k.text("You lost")]);
-})
-
+onKeyPress("left", () => {
+    player.move(100,0);
+});
